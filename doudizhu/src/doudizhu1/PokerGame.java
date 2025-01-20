c1822fdd377ed64f90611898a5cec97132cf1c05
RoyRyy <2524380603@qq.com>
2025-01-21 00:08:51 +0800

    Finish first steps---Poker preparation for three players and landlord.

--- doudizhu/src/doudizhu1/PokerGame.java
+++ doudizhu/src/doudizhu1/PokerGame.java
@@ -1,6 +1,7 @@
 package doudizhu1;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class PokerGame {
 
@@ -26,13 +27,50 @@ public class PokerGame {
     }
 
     public PokerGame(){
-        //准备牌
-        System.out.println(list);
         //洗牌
-
+        Collections.shuffle(list);
         //发牌
+        ArrayList<String> lord=new ArrayList<>();
+        ArrayList<String> player1=new ArrayList<>();
+        ArrayList<String> player2=new ArrayList<>();
+        ArrayList<String> player3=new ArrayList<>();
+
+        //遍历牌盒得到每一张牌
+        for (int i = 0; i < list.size(); i++) {
+            String poker=list.get(i);
+            if(i<=2){
+                lord.add(poker);
+                continue;
+            }
+
+            //给三个玩家轮流发牌
+            if(i%3==0){
+                player1.add(poker);
+            }else if(i%3==1){
+                player2.add(poker);
+            }else if(i%3==2){
+                player3.add(poker);
+            }
+        }
 
         //看牌
+        lookPoker("底牌",lord);
+        lookPoker("蔡徐坤",player1);
+        lookPoker("真爱粉",player2);
+        lookPoker("小黑紫",player3);
+
+    }
+
+    /*
+    参数一：玩家的名字
+    参数二：每位玩家的牌
+     */
+    public void lookPoker(String name,ArrayList<String> list){
+        System.out.println(name+":");
+        for(String poker:list){
+            System.out.print(poker+" ");
+        }
+        System.out.println();
 
     }
 }
